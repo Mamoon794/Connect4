e187d90540f9d4b74de8084ea9574c1865871766
Mamoon Akhtar <primus@Optimus-Primes-Computer.local>
2023-05-02 16:42:57 -0400

    made the game window

---
+++ Connect4Game.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
+public class Connect4Game extends Application implements EventHandler<ActionEvent> {
+
+    private Label text;
+    private final Circle[][] circles = new Circle[7][6];
+    private Pane body;
+    private Scene scene2;
+    private Button[] columns;
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        window = primaryStage;
+        window.setResizable(false);
+        window.centerOnScreen();
+
+        window.setTitle("Connect4");
+    }
+}
